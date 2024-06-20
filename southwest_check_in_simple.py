237042c74316d4e95ecf9e9e3bf3f099e382eedf
Aaron Fiedler <166755904+aaronfiedler@users.noreply.github.com>
2024-06-20 10:36:50 -0400

    Update southwest_check_in_simple.py

--- southwest_check_in_simple.py
+++ southwest_check_in_simple.py
@@ -5,7 +5,8 @@ import pause
 from datetime import datetime
 
 
-
+  ## Enter the info for your CHECK IN 
+  ## don't enter the flight date, enter check in date (24 hours prior)
 enter_confirmation_number = '123456'
 enter_passenger_first_name = 'Aaron'
 enter_passenger_last_name = 'Fiedler'
@@ -16,18 +17,19 @@ check_in_flight_24_hour = 12
 check_in_flight_min = 6
 
 
-
+  ## open webpage
 driver = webdriver.Chrome()
 driver.get('https://www.southwest.com/air/check-in/index.html')
 
+  ## fill in passenger information
 confirmation_number = driver.find_element('id', 'confirmationNumber')
 passenger_first_name = driver.find_element('id', 'passengerFirstName')
 passenger_last_name = driver.find_element('id', 'passengerLastName')
-
 confirmation_number.send_keys(enter_confirmation_number)
 passenger_first_name.send_keys(enter_passenger_first_name)
 passenger_last_name.send_keys(enter_passenger_last_name)
 
+  ## check in
 checkin = driver.find_element('id', 'form-mixin--submit-button')
 pause.until(datetime(check_in_flight_year, check_in_flight_month, check_in_flight_day, check_in_flight_24_hour, check_in_flight_min))
 checkin.click()
